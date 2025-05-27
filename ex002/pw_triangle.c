b9f5383797d12229c225ee39f13f38fbdbc6d07e
Selim Alper <selimalper05@gmail.com>
2025-05-27 16:13:45 +0200

    ex002: implemented pw_triangle - draws ASCII triangle

--- ex002/pw_triangle.c
+++ ex002/pw_triangle.c
@@ -1,15 +1,23 @@
-nano pw_triangle.c
+#include <unistd.h>
 
-nano pw_triangle.c
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
 
-rm pw_triangle.c
-
-
-[A[A[A[B[B[B
-
-
-
-
-
-
+void pw_triangle(void)
+{
+    int i = 1;
+    while (i <= 3)
+    {
+        int j = 0;
+        while (j < i)
+        {
+            ft_putchar('a');
+            j++;
+        }
+        ft_putchar('\n');
+        i++;
+    }
+}
 
