ec8a130f2b2c9e033c40834e1fe6a06744b51a2a
Selim Alper <selimalper05@gmail.com>
2025-05-27 14:39:13 +0200

    Initial commit

---
+++ ex001/pw_hello_powercoders.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+
+void pw_hello_powercoders(void)
+{
+    write(1, "Hello, Powercoders!\n", 20); // Virgül eklendi ve uzunluk 20 oldu
+}
---
+++ ex002/pw_triangle.c
@@ -0,0 +1,15 @@
+nano pw_triangle.c
+
+nano pw_triangle.c
+
+rm pw_triangle.c
+
+
+[A[A[A[B[B[B
+
+
+
+
+
+
+
